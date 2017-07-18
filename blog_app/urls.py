045c668bcb78feefc2ef4b37f0a959440b0fcb27
sabbir <sabbirahmedhimel@gmail.com>
2017-07-18 17:52:31 +0600

    added blog_app working -somewhat!

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
+
 from blog_app import views
 urlpatterns=[
     url(r'^$',views.index,name='index'),
+    url(r'^post/new/$',views.post_new,name='post_new'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,42 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,get_object_or_404
+from .models import Post
+from django.utils import timezone
+from django.contrib.auth.models import User
+import logging
+from .forms import PostForm
+from django.shortcuts import redirect
 
 # Create your views here.
+logger=logging.getLogger(__name__)
+
 def index(request):
-    return render(request,'blog_app/blog_post_list.html',{})
+    posts=Post.objects.all()
+    logger.error("Size of post")
+    return render(request,'blog_app/blog_post_list.html',{'posts':posts})
+
+def post_new(request):
+    if request.method=="POST":
+        form=PostForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+        post.author=request.user
+        post.published_date=timezone.now()
+        post.save()
+        return redirect('post_details',pk=post.pk)
+    else:
+         form=PostForm()  
+    return render(request,'blog_app/post_edit.html',{'form':form})
+
+def post_edit(request,pk):
+    post=get_object_or_404(Post,pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog_app/post_edit.html', {'form': form})
