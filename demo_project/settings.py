91f7ecfca57fde03f1d4689b6d979aff45d5de20
sabbir <sabbirahmedhimel@gmail.com>
2017-08-02 16:02:28 +0600

    added virtualenv support and using mysql database

--- demo_project/settings.py
+++ demo_project/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'demo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
